4b179155540ef50a4e5cb88c8632c62153e289fb
Robert Kooima <robert.kooima@gmail.com>
2012-03-26 14:19:47 +0000

    Oops, forgot process.h.

---
+++ process.h
@@ -0,0 +1,18 @@
+// Copyright (c) 2011 Robert Kooima.  All Rights Reserved.
+
+#ifndef SCMTIFF_PROCESS_H
+#define SCMTIFF_PROCESS_H
+
+//------------------------------------------------------------------------------
+
+int normal (int, char **, const char *, const float *);
+int relink (int, char **);
+int border (int, char **, const char *);
+int mipmap (int, char **, const char *);
+int combine(int, char **, const char *, const char *);
+int convert(int, char **, const char *, const char *,
+            int, int, int, int, int, const double *, const double *, const float *);
+
+//------------------------------------------------------------------------------
+
+#endif
