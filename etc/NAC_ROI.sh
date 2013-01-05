d1cef21081a1b353001dedb6600e55c320190562
Robert Kooima <robert.kooima@gmail.com>
2013-01-05 00:27:58 +0000

    Added some etc.

--- etc/NAC_ROI.sh
+++ etc/NAC_ROI.sh
@@ -9,10 +9,9 @@
 
 tag=$(echo $1 | sed -e 's/NAC_ROI_\(.*\)_.*\.IMG/\1/')
 
+txt="NAC_ROI_$tag-$2-$3.txt"
 tmp="NAC_ROI_$tag-$2-$3-M.tif"
 scm="NAC_ROI_$tag-$2-$3.tif"
-
-txt="desc.txt"
 
 echo "Converting $1 to $scm"
 
@@ -22,6 +21,10 @@ range=$(scmtiff -pextrema $1)
 
 n0=$(echo "$range" | cut -f1)
 n1=$(echo "$range" | cut -f2)
+
+echo "$1 normalized to $n0 $n1"                                > $txt
+echo "Source: LRO-L-LROC-5-RDR-V1.0 ARIZONA STATE UNIVERSITY" >> $txt
+echo "SCM TIFF: Copyright (C) 2012-13 Robert Kooima"          >> $txt
 
 # Convert to SCM TIFF
 
